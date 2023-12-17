8905bdfb1bd8afd8f31335503dc6c0426fbac97b
黄金城02 <hjchndx@henu.edu.cn>
2023-12-17 15:42:58 +0800

    首页、资源文件、和大部分功能性组件

---
+++ test/src/components/content/goods/GoodsList.vue
@@ -0,0 +1,42 @@
+<template>
+<!--商品展示组件，数据导入的终点-->
+  <div class="goods">
+    <GoodsListItem v-for="item in goods" :product="item" :key="item"></GoodsListItem>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import GoodsListItem from './GoodListItem';
+export default defineComponent({
+  name:'GoodsList',
+  components:{
+    GoodsListItem
+  },
+
+  props:{
+    goods:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+
+  setup(){
+
+    return{
+
+    }
+
+  }
+})
+</script>
+
+<style lang='scss'>
+.goods{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 5px;
+}
+</style>
